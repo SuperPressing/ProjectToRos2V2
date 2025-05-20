013098b0c585f2a6cd6d71f4a8bc92a1c660ea06
SuperPressing <rok199200@gmail.com>
2025-05-20 17:27:01 +0300

    Add talk node

--- install/my_robot/share/my_robot/launch/launch_robot.launch.py
+++ install/my_robot/share/my_robot/launch/launch_robot.launch.py
@@ -2,67 +2,61 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
-from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
+from launch.actions import RegisterEventHandler
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
 
+    package_name = 'my_robot'  # Замените на ваше имя пакета
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='my_robot' #<--- CHANGE ME
-
+    # Подключаем robot_state_publisher из rsp.launch.py
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'robot_spawn.launch.py')
+        ]),
+        launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )])
-    # )
-
-
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # Путь к конфигурации twist_mux
+    twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+    )
 
-    
-
-
+    # Получаем описание робота из параметра
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
+    # Путь к конфигурации контроллеров
+    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
 
+    # Запуск ros2_control_node
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+                    controller_params_file],
+        output='screen'
     )
 
+    # Задержка перед запуском контроллера
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
+    # Спавнер дифференциального контроллера
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
+        output='screen'
     )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
@@ -72,10 +66,12 @@ def generate_launch_description():
         )
     )
 
+    # Спавнер joint broadcaster
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
+        output='screen'
     )
 
     delayed_joint_broad_spawner = RegisterEventHandler(
@@ -85,31 +81,10 @@ def generate_launch_description():
         )
     )
 
-
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
-
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
-        # joystick,
-        twist_mux,
-        delayed_controller_manager,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner
+        #twist_mux,
+        #delayed_controller_manager,
+        #delayed_diff_drive_spawner,
+        #delayed_joint_broad_spawner
     ])
--- install/my_robot/share/my_robot/launch/launch_sim.launch.py
+++ install/my_robot/share/my_robot/launch/launch_sim.launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
+from launch.event_handlers import OnProcessStart, OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
@@ -12,27 +12,35 @@ from launch_ros.actions import Node
 def generate_launch_description():
     package_name = 'my_robot'
 
-    # Подключаем robot_state_publisher из rsp.launch.py
+    # Подключаем robot_state_publisher
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory(package_name), 'launch', 'launch_robot.launch.py')
-        ]),
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]
+        ),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Запуск Gazebo
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
-    world_path = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
+    # Создаем Node для robot_state_publisher отдельно
+    # robot_state_publisher_node = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     name='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{
+    #         'use_sim_time': True
+    #     }]
+    # )
 
+    # Подключаем Gazebo
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    world_path = os.path.join(get_package_share_directory(package_name), 'worlds', 'WareHouseV2.world')
+    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-        ]),
-        launch_arguments={
-            'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file,
-            'world': world_path
-        }.items()
-    )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file,
+                                      'world': world_path}.items()
+             )
 
     # Спавним модель робота в Gazebo
     spawn_entity = Node(
@@ -43,9 +51,10 @@ def generate_launch_description():
     )
 
     # Контроллеры
+    
     diff_drive_spawner = ExecuteProcess(
-        cmd=['ros2', 'run', 'controller_manager', 'spawner', 'diff_cont'],
-        output='screen'
+    cmd=['ros2', 'run', 'controller_manager', 'spawner', 'diff_cont'],
+    output='screen'
     )
 
     joint_broad_spawner = ExecuteProcess(
@@ -53,25 +62,16 @@ def generate_launch_description():
         output='screen'
     )
 
-    # SLAM Toolbox
-    slam_toolbox_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory("slam_toolbox"), 'launch', 'online_async_launch.py')
-        ]),
-        launch_arguments={
-            'use_sim_time': 'true',
-            'params_file': os.path.join(get_package_share_directory(package_name), 'config', 'mapper_params_online_async.yaml')
-        }.items()
-    )
-
     # === РЕГИСТРАЦИЯ ОБРАБОТЧИКОВ СОБЫТИЙ ===
-    delayed_slam_toolbox = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[slam_toolbox_node],
-        )
-    )
+    # Спавним модель после запуска robot_state_publisher_node
+    # delayed_spawn_entity = RegisterEventHandler(
+    #     event_handler=OnProcessStart(
+    #         target_action=robot_state_publisher_node,
+    #         on_start=[spawn_entity],
+    #     )
+    # )
 
+    # diff_drive после спавна модели
     delayed_diff_drive_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=spawn_entity,
@@ -79,6 +79,7 @@ def generate_launch_description():
         )
     )
 
+    # joint_broad после diff_drive
     delayed_joint_broad_spawner = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=diff_drive_spawner,
@@ -86,12 +87,50 @@ def generate_launch_description():
         )
     )
 
-    # === LAUNCH DESCRIPTION ===
+    # return LaunchDescription([
+    #     # Добавляем основные действия
+    #     robot_state_publisher_node,
+    #     gazebo,
+
+    #     # Регистрируем обработчики событий
+    #     delayed_spawn_entity,
+    #     delayed_diff_drive_spawner,
+    #     delayed_joint_broad_spawner,
+    # ])
+    # === ДОБАВЛЯЕМ SLAM TOOLBOX С ЗАДЕРЖКОЙ ===
+    slam_toolbox_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory("slam_toolbox"), 'launch', 'online_async_launch.py')]
+        ),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'params_file': os.path.join(get_package_share_directory(package_name), 'config', 'mapper_params_online_async.yaml')
+        }.items()
+    )
+
+    # Запускаем SLAM после `joint_broad`
+    delayed_slam_toolbox = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[slam_toolbox_node],
+        )
+    )
+    map = Node(
+        package='editing_map',
+        executable='talker',
+        name='minimal_publisher',
+        output='screen'
+    )
+    # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
+        # twist_mux,
+        #robot_state_publisher_node,
         gazebo,
         spawn_entity,
         delayed_slam_toolbox,
-        delayed_diff_drive_spawner,
-        delayed_joint_broad_spawner,
+        map,
+        #dlayed_diff_drive_spawner,
+        #delayed_joint_broad_spawner,
     ])
